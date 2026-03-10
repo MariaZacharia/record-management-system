4a62a3aec9fda72e8e2480e16a2d21c22237c897
nepryoon <lilloluca@gmail.com>
2026-03-10 18:59:09 +0100

    feat: add custom exceptions for repository error handling

---
+++ src/exceptions.py
@@ -0,0 +1,8 @@
+class RecordNotFoundError(Exception):
+    """Raised when a record cannot be found in the repository."""
+    pass
+
+
+class DuplicateRecordError(Exception):
+    """Raised when attempting to add a record that already exists."""
+    pass
